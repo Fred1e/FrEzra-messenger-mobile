e01030ccd0e8672684e72009aadfec0cb7789137
FredieTech <frediezra60@gmail.com>
2025-03-29 14:25:13 +0300

    Create Settings.js

---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Settings.css";
+
+const Settings = () => {
+  return (
+    <div className="settings-container">
+      <h2>Settings</h2>
+      <p>Dark Mode: <button>Toggle</button></p>
+    </div>
+  );
+};
+
+export default Settings;
