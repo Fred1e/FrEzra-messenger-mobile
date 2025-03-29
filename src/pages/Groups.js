9eb367b0be5951679843ef0da8ae77712afb2e2e
FredieTech <frediezra60@gmail.com>
2025-03-29 14:21:03 +0300

    Create Groups.js

---
+++ src/pages/Groups.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Groups.css";
+
+const Groups = () => {
+  return (
+    <div className="groups-container">
+      <h2>Groups</h2>
+      <p>No groups yet. Create one now!</p>
+    </div>
+  );
+};
+
+export default Groups;
